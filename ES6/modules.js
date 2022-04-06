bc12e1a54245a6d66708b6a73f2fc4ca5adcaff4
Haridharshini.S <94096479+HARIDHARSHINI-17@users.noreply.github.com>
2022-04-07 00:25:08 +0530

    Add files via upload
    06/04/2022

---
+++ ES6/modules.js
@@ -0,0 +1,12 @@
+
+export class myclass{
+      constructor(name,age){
+          this.name = name;
+          this.age = age;
+
+      }
+}
+var obj = new myclass("hari",20);
+obj.name();
+obj.age();
+

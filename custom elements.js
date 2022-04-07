474660207e65b5522afbdb36978eafc6c7f2951b
Haridharshini.S <94096479+HARIDHARSHINI-17@users.noreply.github.com>
2022-04-07 23:48:30 +0530

    Delete custom elements.js

--- custom elements.js
+++
@@ -1,6 +0,0 @@
-function touchHandler(event) {
-    // Get a reference to our coordinates div
-    var coords = document.getElementById("coords");
-    // Write the coordinates of the touch to the div
-    coords.innerHTML = 'x: ' + event.touches[0].pageX + ', y: ' + event.touches[0].pageY;
-  }
